caf16538371621451cf42089f68d79560ae35b4b
jack <jack1824871823@163.com>
2026-07-14 00:11:38 +0800

    个人博客1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://alex-blog.example",
+  site: "https://jackblog.pages.dev",
   integrations: [
     tailwind({
       applyBaseStyles: false
